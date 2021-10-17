b25d722b61819278d6b074e1e02508d474b3c5ab
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-17 22:19:17 +0800

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultRadius = 6.0;
+double defaultMargin = 30.0;
+
+Color kPrimaryColor = Color(0xff5284E3);
+Color kSecondaryColor = Color(0xff292A2D);
+Color kBackgroundColor1 = Color(0xffFFFFFF);
+Color kBlackColor = Color(0xff292A2D);
+Color kGreyColor = Color(0xffE5E5E5);
+
+TextStyle primaryTextStyle = GoogleFonts.mulish(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.mulish(
+  color: kBackgroundColor1,
+);
+
+TextStyle greyTextStyle = GoogleFonts.mulish(
+  color: kGreyColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.mulish(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
