f63ff89a1925cf0d483157f4871bbcde66b54e1c
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-18 16:56:35 +0800

    update

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:jobby_app/shared/theme.dart';
 import 'package:jobby_app/widgets/custom_button.dart';
 import 'package:jobby_app/widgets/input_custom.dart';
@@ -62,7 +64,79 @@ class RegisterPage extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: defaultMargin,
         ),
-        child: CustomButton(title: 'Register'),
+        child: Column(
+          children: [
+            CustomButton(title: 'Register'),
+            SizedBox(height: defaultMargin),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: defaultMargin,
+              ),
+              child: Text(
+                'or continue with',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ),
+
+            /// NOTE: BUTTON GOOGLE
+            Container(
+              width: double.infinity,
+              height: 64,
+              child: OutlinedButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: kBackgroundColor1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/google_icon.png', width: 20),
+                    SizedBox(
+                      width: 7,
+                    ),
+                    Text(
+                      'Google Account',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 124,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already have an account?',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Text(
+                  'Sign in',
+                  style: blueTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 74)
+          ],
+        ),
       );
     }
 
